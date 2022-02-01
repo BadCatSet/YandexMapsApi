077c1e94170e88453bfa506c045dacaaee94c1de
petr78ya <petr.78ya@gmail.com>
2022-02-01 17:37:51 +0300

    \/ почти готово

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import sys
+from io import BytesIO
 
-from PyQt5 import Qt, uic
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
 import requests
+from PIL import Image
+from PyQt5 import Qt, uic
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
 
 
 class MainWindow(QMainWindow):
@@ -12,8 +15,20 @@ class MainWindow(QMainWindow):
         super().__init__()
         uic.loadUi('main_window.ui')
         self.zoom = 5
-        self.delta_press = 0.00001
+        self.delta = 0.00001
         self.lantitude = 90
+        self.ll = '37.677751,55.757718'
+        self.map_params = {
+            "ll": self.ll,
+            "spn": f'{self.delta},{self.delta}',
+            "l": "map"
+        }
+        self.initUI()
+        self.get_map()
+
+    def initUI(self):
+        self.setGeometry(100, 100, *(600, 400))
+        self.setWindowTitle('Отображение карты')
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_PageUp:
@@ -24,7 +39,14 @@ class MainWindow(QMainWindow):
                 self.zoom -= 1
 
     def get_map(self):
-        response = requests.get('https://static-maps.yandex.ru/1.x/', params={'z': self.z})
+        response = requests.get('https://static-maps.yandex.ru/1.x/',
+                                params=self.map_params)
+
+        self.pixmap = QPixmap(Image.open(BytesIO(response.content)))
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
+        self.image.setPixmap(self.pixmap)
 
 
 app = QApplication(sys.argv)
